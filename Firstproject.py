e9ae9460dda5555788d362c53c916402e91a01cf
Corryv <105764291+Corryv@users.noreply.github.com>
2022-05-17 22:59:18 -0400

    Add files via upload

---
+++ Firstproject.py
@@ -0,0 +1,9 @@
+#FirstProjectbyCorry
+#May17th2022
+#FahrenheittoCelcius
+
+
+temp = float (input('What is the current temperature?: '))
+cel =float((temp - 32) * 5 / 9)
+cel = round(cel,2)
+print(cel, 'celcius')
